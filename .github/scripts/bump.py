1e15a84d801a81eb9f71d4aba3794b2049752930
michalby24 <michalby24@gmail.com>
2024-01-06 16:44:38 +0200

    fix: 67

---
+++ .github/scripts/bump.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+
+def has_helm_files_changed():
+    result = subprocess.run(
+        ["git", "diff", "--name-only", "HEAD@{1}", "HEAD", "--", "helm/"],
+        capture_output=True,
+        text=True,
+    )
+    
+    return bool(result.stdout.strip())
+
+def bump_version():
+    subprocess.run(["git", "checkout", "-qf", "master"])
+
+    version_result = subprocess.run(
+        ["awk", "/version/{print $2; exit}", "./helm/Chart.yaml"],
+        capture_output=True,
+        text=True,
+        cwd="./helm",
+    )
+
+    current_version = version_result.stdout.strip()
+
+    new_version_result = subprocess.run(
+        [
+            "awk",
+            "-F.",
+            "-v",
+            "OFS=",
+            '{ $NF++; print }',
+        ],
+        input=current_version,
+        capture_output=True,
+        text=True,
+    )
+
+    new_version = new_version_result.stdout.strip()
+
+    subprocess.run(
+        [
+            "stefanzweifel/git-auto-commit-action@v4",
+            "--commit-message",
+            f"chore(release) bump version to {new_version}",
+            "--file-pattern",
+            "./helm/Chart.yaml",
+        ]
+    )
+
+    return new_version
+
+if __name__ == "__main__":
+    if has_helm_files_changed():
+        new_version = bump_version()
+        print(f"Bumped version to {new_version}")
+    else:
+        print("No changes in the helm directory.")
