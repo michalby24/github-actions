65c66f83f9a11089f9ffd961e21869335dbe74e0
michalby24 <michalby24@gmail.com>
2024-01-07 15:25:32 +0200

    fix 32

--- .github/scripts/bump.py
+++ .github/scripts/bump.py
@@ -23,7 +23,7 @@ def get_chart_version():
 
     return ""
 
-def bump_version_segments(version, condition_index=2):
+def bump_version_segments2(version, condition_index=2):
     # Split the version into segments
     segments = version.split(".")
 
@@ -39,7 +39,7 @@ def bump_version_segments(version, condition_index=2):
         print(f"Invalid condition_index: {condition_index}")
         return version
 
-def change_version_in_chart2(new_version):
+def change_version_in_chart(new_version):
     chart_path = os.path.join("helm", "Chart.yaml")
     github_token = os.environ.get("GITHUB_TOKEN") 
     # Use sed to replace the version line
@@ -53,10 +53,33 @@ def change_version_in_chart2(new_version):
     subprocess.run(["git", "push", "origin", "master"])
 
 
+def bump_version_segments(version, condition_index=2):
+    # Split the version into segments
+    segments = list(map(int, version.split(".")))
+
+    # Check if the condition_index is within the valid range
+    if 0 <= condition_index < len(segments):
+        # Increment the specified segment by 1
+        segments[condition_index] += 1
+
+        # Check if the incremented segment reaches 9, reset to 0 and bump the next segment
+        if segments[condition_index] == 9:
+            segments[condition_index] = 0
+            if condition_index + 1 < len(segments):
+                segments[condition_index + 1] += 1
+
+        # Convert segments back to string and join to get the new version
+        new_version = ".".join(map(str, segments))
+        return new_version
+    else:
+        print(f"Invalid condition_index: {condition_index}")
+        return version
+
+
 if __name__ == "__main__":
     if True:
         new_version = bump_version_segments(get_chart_version(), 2) 
-        change_version_in_chart2(new_version)
+        change_version_in_chart(new_version)
         print(f"Bumped version to {new_version}")
     else:
         print("No changes in the helm directory.")
