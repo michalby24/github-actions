da06579b05d9ef2c69466a440a72a171ab4a40c7
michalby24 <michalby24@gmail.com>
2024-01-07 15:39:18 +0200

    fix 35

--- .github/scripts/bump.py
+++ .github/scripts/bump.py
@@ -55,25 +55,23 @@ def change_version_in_chart(new_version):
 
 def bump_version_segments(version, condition_index=2):
     # Split the version into segments
-    segments = list(map(int, version.split(".")))
+    major, minor, patch = map(int, version.split("."))
 
-    # Check if the condition_index is within the valid range
-    if 0 <= condition_index < len(segments):
-        # Increment the specified segment by 1
-        segments[condition_index] += 1
+    # Increment the patch by 1
+    patch += 1
 
-        # Check if the incremented segment reaches 9, reset to 0 and bump the next segment
-        if segments[condition_index] == 10:
-            segments[condition_index] = 0
-            if condition_index + 1 < len(segments):
-                segments[condition_index + 1] += 1
+    # Check if the patch reaches 10, reset to 0 and increment the minor by 1
+    if patch == 10:
+        patch = 0
+        minor += 1
 
-        # Convert segments back to string and join to get the new version
-        new_version = ".".join(map(str, segments))
-        return new_version
-    else:
-        print(f"Invalid condition_index: {condition_index}")
-        return version
+        # Check if the minor reaches 10, reset to 0 and increment the major by 1
+        if minor == 10:
+            minor = 0
+            major += 1
+
+    new_version = f"{major}.{minor}.{patch}"
+    return new_version
 
 
 if __name__ == "__main__":
