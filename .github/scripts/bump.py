2f95f3becc834859a497e7a98bd42f8d723eb7c9
michalby24 <michalby24@gmail.com>
2024-01-08 11:18:12 +0200

    change minor things

--- .github/scripts/bump.py
+++ .github/scripts/bump.py
@@ -9,24 +9,23 @@ def get_chart_version():
             if line.startswith("version:"):
                 version = line.split(":")[1].strip()
                 return version
+                
+    raise Exception('Error with chart version')
 
-    return ""
-
-def change_version_in_chart(new_version):
+def update_chart_version(new_version):
     chart_path = os.path.join("helm", "Chart.yaml")
     github_token = os.environ.get("GITHUB_TOKEN") 
     # Use sed to replace the version line
     subprocess.run(["sed", "-i", f"s/^version: .*/version: {new_version}/", chart_path])
+    # TODO: check if really necessary 
     subprocess.run(["git", "config", "--global", "user.email", "github-actions@github.com"])
     subprocess.run(["git", "config", "--global", "user.name", "GitHub Actions"])
     subprocess.run(["git", "add", chart_path])
     subprocess.run(["git", "commit", "-m", f"chore(release): bump version to {new_version}"])
-    subprocess.run(["git", "fetch"])
-    subprocess.run(["git", "merge", "--ff-only", "origin/master"])
     subprocess.run(["git", "push", "origin", "master"])
 
 
-def bump_version_segments(version, condition_index=2):
+def generate_new_version(version):
     # Split the version into segments
     major, minor, patch = map(int, version.split("."))
 
@@ -44,9 +43,6 @@ def bump_version_segments(version, condition_index=2):
     return new_version
 
 if __name__ == "__main__":
-    if True:
-        new_version = bump_version_segments(get_chart_version(), 2) 
-        change_version_in_chart(new_version)
-        print(f"Bumped version to {new_version}")
-    else:
-        print("No changes in the helm directory.")
+    new_version = bump_version_segments(get_chart_version())
+    change_version_in_chart(new_version)
+    print(f"Bumped version to {new_version}")
