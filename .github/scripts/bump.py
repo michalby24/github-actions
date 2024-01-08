3cfeb4afc263272a37dba2c24670031bdbf82fb7
michalby24 <michalby24@gmail.com>
2024-01-08 13:20:37 +0200

    MIC

--- .github/scripts/bump.py
+++ .github/scripts/bump.py
@@ -47,6 +47,5 @@ if __name__ == "__main__":
     new_version = generate_new_version(get_chart_version())
     update_chart_version(new_version)
     print(f"Bumped version to {new_version}")
-    print(f"::set-env name=NEW_VERSION::{new_version}")
-
+    print(f"::set-output name=NEW_VERSION::{new_version}")
 
