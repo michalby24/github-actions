39825ffe717f4419094c7c5295eaf714c18af586
michalby24 <michalby24@gmail.com>
2024-02-06 15:56:32 +0200

    8

---
+++ .github/scripts/bump copy.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+def update_chart_version(new_version):
+    commands = f"""
+        sed -i 's/^version:.*/version: {new_version}/' {chart_path};
+        sed -i 's/^appVersion:.*/appVersion: {new_version}/' {chart_path};
+        git config --global user.email 'github-actions@github.com';
+        git config --global user.name 'GitHub Actions';
+        git add {chart_path};
+        git commit -m 'chore(release): bump version to {new_version}';
+        git push origin master;
+    """
+    subprocess.run(commands, shell=True)
+
+if __name__ == "__main__":
+    new_version = generate_new_version(get_chart_version())
+    update_chart_version(new_version)
+    print(f"Bumped version to {new_version}")
+    print(f"::set-output name=NEW_VERSION::{new_version}")
+
