7059f180c2a5a91d1feb155de1741f574efc7a1e
michalby24 <michalby24@gmail.com>
2024-02-07 13:04:51 +0200

    47

--- .github/scripts/bump-copy.py
+++ .github/scripts/bump-copy.py
@@ -70,6 +70,7 @@ def generate_new_version(version):
 
 if __name__ == "__main__":
     new_version = os.environ.get('VERSION')
+    print(new_version)
     update_chart_version(new_version)
     print(f"Bumped version to {new_version}")
     print(f"::set-output name=NEW_VERSION::{new_version}")
