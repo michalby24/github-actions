7635ac0a5d2834809acfa246f3d7bcfae325fd1a
michalby24 <michalby24@gmail.com>
2024-02-12 10:22:51 +0200

    149

--- .github/scripts/bump-copy.py
+++ .github/scripts/bump-copy.py
@@ -1,39 +1,56 @@
 import os
 import subprocess
 
+import json
+
 def get_chart_version():
     path = 'package.json'
 
     with open(path, "r") as file:
-        for line in file:
-            if '"version"' in line:
-                print("Line detected:", line.strip())
-                parts = line.split(":")
-                if len(parts) >= 2:
-                    #print(parts[1])
-                    version = parts[1]
-                    #print("Extracted version:", version)
-                    #print(version)
-                    return version
-                #else:
-                    #print("Unexpected line format:", line.strip())
-        #print("No line starting with 'version' found.")
-    return None
+        data = json.load(file)
+        version = data.get('version')
 
-                
+        if version:
+            print("Extracted version:", version)
+            return version
+        else:
+            print("No 'version' key found in package.json.")
+            return None
+
     raise Exception('Error with chart version')
+
+
+# def get_chart_version():
+#     path = 'package.json'
+
+#     with open(path, "r") as file:
+#         for line in file:
+#             if '"version"' in line:
+#                 print("Line detected:", line.strip())
+#                 parts = line.split(":")
+#                 if len(parts) >= 2:
+#                     print(parts[1])
+#                     version = parts[1]
+#                     print("Extracted version:", version)
+#                     print(version)
+#                     return version
+#                 else:
+#                     print("Unexpected line format:", line.strip())
+#         print("No line starting with 'version' found.")
+#     return None
+
+#     raise Exception('Error with chart version')
 
 def update_chart_version(new_version):
     print(new_version)
     chart_path = os.path.join("helm", "Chart.yaml")
     commands = f"""
-        version_1=$(echo {new_version} | tr -d ',' | tr -d '"')
-        sed -i 's/^version:.*/version: $version_1/' {chart_path};
-        sed -i 's/^appVersion:.*/appVersion: $version_1/' {chart_path};
+        sed -i 's/^version:.*/version: {new_version}/' {chart_path};
+        sed -i 's/^appVersion:.*/appVersion: {new_version}/' {chart_path};
         git config --global user.email 'github-actions@github.com';
         git config --global user.name 'GitHub Actions';
         git add {chart_path};
-        git commit -m 'chore(release): bump version to $version_1';
+        git commit -m 'chore(release): bump version to {new_version}';
         git push origin master;
     """
 
