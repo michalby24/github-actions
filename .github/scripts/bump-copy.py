ff3de60692ecd1de00271f714701e0c065180fa5
michalby24 <michalby24@gmail.com>
2024-02-12 11:47:31 +0200

    155

--- .github/scripts/bump-copy.py
+++ .github/scripts/bump-copy.py
@@ -19,34 +19,12 @@ def get_chart_version():
 
     raise Exception('Error with chart version')
 
-
-# def get_chart_version():
-#     path = 'package.json'
-
-#     with open(path, "r") as file:
-#         for line in file:
-#             if '"version"' in line:
-#                 print("Line detected:", line.strip())
-#                 parts = line.split(":")
-#                 if len(parts) >= 2:
-#                     print(parts[1])
-#                     version = parts[1]
-#                     print("Extracted version:", version)
-#                     print(version)
-#                     return version
-#                 else:
-#                     print("Unexpected line format:", line.strip())
-#         print("No line starting with 'version' found.")
-#     return None
-
-#     raise Exception('Error with chart version')
-
 def update_chart_version(new_version):
     print(new_version)
     chart_path = os.path.join("helm", "Chart.yaml")
     commands = f"""
-        sed -i 's/^version:.*/version: v{new_version}/' {chart_path};
-        sed -i 's/^appVersion:.*/appVersion: v{new_version}/' {chart_path};
+        sed -i 's/^version:.*/version: {new_version}/' {chart_path};
+        sed -i 's/^appVersion:.*/appVersion: {new_version}/' {chart_path};
         git config --global user.email 'github-actions@github.com';
         git config --global user.name 'GitHub Actions';
         git add {chart_path};
@@ -60,16 +38,8 @@ if __name__ == "__main__":
     new_version =  os.environ['VERSION']
     
     if(new_version == ''):
-    #     update_chart_version(new_version)
-    # else:
-        # update_chart_version(get_chart_version())
         new_version=get_chart_version()
-    
-    
-    # michs = get_chart_version()
-    # print(michs.strip('"'))
-    # update_chart_version(michs)
-    # update_chart_version("2.8.2")
+
     update_chart_version(new_version)
     print(f"Bumped version to {new_version}")
     print(f"::set-output name=NEW_VERSION::{new_version}")
